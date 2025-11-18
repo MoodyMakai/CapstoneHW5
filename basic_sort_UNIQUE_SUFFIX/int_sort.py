03b7d72377f8fbed0677fa4bed55cbe36df95e97
Corey Aras Kaulenas <corey.kaulenas@gmail.com>
2025-11-17 23:11:38 -0500

    Quick and insertions sort

--- basic_sort_UNIQUE_SUFFIX/int_sort.py
+++ basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -17,15 +17,19 @@
 # =========================================================================
 
 """
-This module sorts lists of integers...
+This module provide functions to sort a list of integers in many ways.
+It implements, bubble, quick, and insertion sort in their respective functions.
+Pass a list of only integers into any of these functions and it will sort that list.
 """
 
 
 def bubble(int_list):
     """
-    :param int_list: the list of only integers to be sorted
+    A function that sorts a given list of integers using bubble sort.
+
+    :param int_list: the list of only integers to be sorted.
     """
-    length = range(int_list)
+    length = len(int_list)
     for i in range(length):
         swapped = False
         for j in range(length - i - 1):
@@ -36,14 +40,48 @@ def bubble(int_list):
             break
 
 
+def _quick_recursive(arr_part, low, high):
+    """
+    A helper function for quick that allows it to be recursive. Not part of the api.
+
+    :param arr_part: The partition of the list you are working on.
+    :param low: The lower bound for the partition.
+    :param high: The upper bound for the partition.
+    """
+    if low < high:
+        pi = arr_part[high]
+        i = low - 1
+        for j in range(low, high):
+            if arr_part[j] < pi:
+                i += 1
+                arr_part[i], arr_part[j] = arr_part[j], arr_part[i]
+        arr_part[i + 1], arr_part[high] = arr_part[high], arr_part[i + 1]
+        pivot = i + 1
+        _quick_recursive(arr_part, low, pivot - 1)
+        _quick_recursive(arr_part, pivot + 1, high)
+
+
 def quick(int_list):
     """
-    :param int_list: the list of only integers to be sorted
+    A function that sorts a given list of integers using quick sort.
+
+    :param int_list: the list of only integers to be sorted.
     """
+    low = 0
+    high = len(int_list) - 1
+    _quick_recursive(int_list, low, high)
 
 
 def insertion(int_list):
     """
-    insertion docstring
+    A function that sorts a given list of integers using quick sort.
+
+    :param int_list: the list of only integers to be sorted.
     """
-    print("insertion sort")
+    for i in range(1, len(int_list)):
+        key = int_list[i]
+        j = i - 1
+        while j >= 0 and key < int_list[j]:
+            int_list[j + 1] = int_list[j]
+            j -= 1
+        int_list[j + 1] = key
